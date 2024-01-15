914d4145ebcc77a7d591402bbd7dabc7056d76fd
Hrushikesh Borhade <hrushiborhade123@gmail.com>
2024-01-15 11:39:26 +0530

    functions in golang

--- Functions.go
+++ Functions.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
-func main(){
-fmt.Printf("Hello world")
+func main() {
+	sendsSoFar := 430
+	const sendsToAdd = 25
+	incrementSends(sendsSoFar, sendsToAdd)
+	fmt.Println("you've sent", sendsSoFar, "messages")
+
+// ignoring return values
+	firstName, _ := getNames()
+	fmt.Println("Welcome to Textio,", firstName)
+}
+
+func incrementSends(sendsSoFar, sendsToAdd int) int {
+	sendsSoFar = sendsSoFar + sendsToAdd
+	return sendsSoFar
+}
+
+func getNames() (string, string) {
+	return "John", "Doe"
+}
+
+//Named Returns
+func calculator(a, b int) (mul, div int, err error) {
+	if b == 0 {
+		return 0, 0, errors.New("Can't divide by zero")
+	}
+	mul = a * b
+	div = a / b
+	return mul, div, nil
 }
